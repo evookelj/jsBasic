7926e5003a116ea0c72f6a083344d9319382180c
eVookelj <evukelj@stuy.edu>
2016-12-15 12:53:03 -0500

    Do removal

--- intro.js
+++ intro.js
@@ -1,3 +1,4 @@
+//========================Point 1
 var newElem = function(e) {
     var l = document.getElementById("thelist");
     var c = l.appendChild(document.createElement("li"));
@@ -6,10 +7,22 @@ var newElem = function(e) {
 
 b.addEventListener( 'click', newElem);
 
+//========================Point 2
 var lists = document.getElementsByTagName("li");
 for (var i=0; i<lists.length; i++) {
     //doesn't yet work b/c 'hover' isnt an eventg
     lists[i].addEventListener('hover',function(e){
 	console.log(this);
+    });
+}
+
+//========================Point 3
+
+//========================Point 4
+var lists = document.getElementsByTagName("li");
+var list = document.getElementById("thelist");
+for (var i=0; i<lists.length; i++) {
+    lists[i].addEventListener('click',function(e){
+	this.remove()
     });
 }
